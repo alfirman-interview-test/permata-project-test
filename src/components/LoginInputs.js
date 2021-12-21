c21a8ab5e4d26f7be6038c45841e6cf4c60d634e
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2021-12-21 18:48:44 +0700

    feat: login functionality

--- src/components/LoginInputs.js
+++ src/components/LoginInputs.js
@@ -1,16 +1,32 @@
-export default function LoginInputs() {
+export default function LoginInputs({ handleChange, input }) {
   return (
     <>
       <div className="space-y-1">
-        <label className="text-gray-500 text-sm font-semibold">Email</label>
+        <label
+          htmlFor="username"
+          className="text-gray-500 text-sm font-semibold"
+        >
+          Email
+        </label>
         <input
+          id="username"
+          value={input.username}
+          onChange={(e) => handleChange(e)}
           placeholder="Masukkan Email Anda"
           className="w-full px-3 py-2 rounded border-2 border-gray-300 focus:outline-none shadow-sm"
         />
       </div>
       <div className="space-y-1 mt-4">
-        <label className="text-gray-500 text-sm font-semibold">Password</label>
+        <label
+          htmlFor="password"
+          className="text-gray-500 text-sm font-semibold"
+        >
+          Password
+        </label>
         <input
+          id="password"
+          value={input.password}
+          onChange={(e) => handleChange(e)}
           placeholder="Masukkan Password Anda"
           className="w-full px-3 py-2 rounded border-2 border-gray-300 focus:outline-none shadow-sm"
         />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,46 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import GoToRegister from "../components/GoToRegister";
 import LoginInputs from "../components/LoginInputs";
 import RememberMe from "../components/RememberMe";
 
 export default function Login() {
+  const { push } = useHistory();
+  const [isLoading, setLoading] = useState(false);
+  const [input, setInput] = useState({
+    username: "johndoe",
+    password: "password",
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    fetch("https://tasklogin.herokuapp.com/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+        if (data.code === 200) push("/");
+      })
+      .catch((e) => console.log("error", e))
+      .finally(() => setLoading(false));
+  };
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.id]: e.target.value });
+  };
+
   return (
     <main className="flex justify-center items-center min-h-screen">
       <div className="w-[400px]">
         <h4 className="text-center font-bold text-2xl text-gray-700">
           Silahkan masuk ke akun anda
         </h4>
-        <form onSubmit={(e) => e.preventDefault()} className="mt-10">
-          <LoginInputs />
+        <form onSubmit={(e) => submit(e)} className="mt-10">
+          <LoginInputs handleChange={handleChange} input={input} />
           <div className="flex justify-end mt-2">
             <button className="text-pink-500 font-semibold">
               Lupa password?
@@ -19,9 +49,14 @@ export default function Login() {
           <RememberMe />
           <button
             type="submit"
-            className="w-full py-2 bg-emerald-400 hover:bg-emerald-500 my-4 text-white rounded"
+            disabled={isLoading}
+            className={`${
+              isLoading
+                ? "cursor-not-allowed bg-gray-300 text-gray-500"
+                : "bg-emerald-400 hover:bg-emerald-500 text-white"
+            } w-full my-4 py-2 rounded`}
           >
-            Masuk
+            {isLoading ? "Loading..." : "Masuk"}
           </button>
           <GoToRegister />
         </form>
